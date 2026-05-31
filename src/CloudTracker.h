2ed5880ea8f405fb781bd6e3c325e6e46c58b33a
ARAVINDSANKARS64 <aravindsankars64@gmail.com>
2026-05-31 12:58:18 +0530

    Updated project

--- src/CloudTracker.h
+++ src/CloudTracker.h
@@ -7,40 +7,41 @@
 
 class CloudTracker {
 public:
-    CloudTracker();
-    ~CloudTracker();
+  CloudTracker();
+  ~CloudTracker();
 
-    // Loads two satellite images from given paths
-    bool loadImages(const std::string& path1, const std::string& path2);
+  // Loads two satellite images from given paths
+  bool loadImages(const std::string &path1, const std::string &path2);
 
-    // Processes the images: thresholding, masking, optical flow, and wind speed estimation
-    // timeDifference is in hours (or any arbitrary unit depending on satellite pass frequency)
-    void process(double timeDifference);
+  // Processes the images: thresholding, masking, optical flow, and wind speed
+  // estimation timeDifference is in hours (or any arbitrary unit depending on
+  // satellite pass frequency)
+  void process(double timeDifference);
 
-    // Display the results in OpenCV windows
-    void showResults();
+  // Display the results in OpenCV windows
+  void showResults();
 
 private:
-    cv::Mat m_img1_color, m_img2_color;
-    cv::Mat m_img1_gray, m_img2_gray;
-    cv::Mat m_cloud_mask1, m_cloud_mask2;
-    cv::Mat m_highlighted_clouds;
-    cv::Mat m_map_overlay; // Stores static country borders and text
-    cv::Mat m_flow;
+  cv::Mat m_img1_color, m_img2_color;
+  cv::Mat m_img1_gray, m_img2_gray;
+  cv::Mat m_cloud_mask1, m_cloud_mask2;
+  cv::Mat m_highlighted_clouds;
+  cv::Mat m_map_overlay; // Stores static country borders and text
+  cv::Mat m_flow;
 
-    cv::Mat m_wind_speed_map;
-    cv::Mat m_flow_visualization;
+  cv::Mat m_wind_speed_map;
+  cv::Mat m_flow_visualization;
 
-    double m_cloud_coverage_percentage;
-    double m_avg_wind_speed;
-    
-    // Internal helper functions
-    cv::Mat preprocessAndMask(const cv::Mat& src_gray, cv::Mat& out_mask);
-    void extractOverlays();
-    void computeOpticalFlow();
-    void estimateWindSpeed(double timeDifference);
-    void drawMotionVectors(int step, const cv::Scalar& color);
-    void drawLegend(cv::Mat& image);
+  double m_cloud_coverage_percentage;
+  double m_avg_wind_speed;
+
+  // Internal helper functions
+  cv::Mat preprocessAndMask(const cv::Mat &src_gray, cv::Mat &out_mask);
+  void extractOverlays();
+  void computeOpticalFlow();
+  void estimateWindSpeed(double timeDifference);
+  void drawMotionVectors(int step, const cv::Scalar &color);
+  void drawLegend(cv::Mat &image);
 };
 
 #endif // CLOUDTRACKER_H
